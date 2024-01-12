e2245e9811d23e9b0c6ce5f96a1e00926fd10b75
Fidan Heydarli <fidanheydarli@Fidans-MacBook-Pro.local>
2024-01-12 13:28:58 +0100

    This is my projects

---
+++ Web_development_project/12. Backend/2.2 Native Modules/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+fs.readFile("message.txt", function () {
+  console.log("it has ben read");
+  //   var result = data.replace("Hello, World", "Fidan Heydarli");
+  //   fs.writeFile("message.txt", result, function (err) {
+  //     console.log(err);
+  //   });
+});
---
+++ Web_development_project/12. Backend/2.3 NPM/index.js
@@ -0,0 +1,4 @@
+import superheroes from "superheroes";
+
+var myName = superheroes;
+console.log("My name is " + myName.random());
